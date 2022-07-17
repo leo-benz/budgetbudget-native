8df3f7e85071ba26c177ccde75b9e441e3b7bb46
Leo Benz <git@l30.me>
2022-07-17 19:44:08 +0200

    feat: initial commit
    - Setup XCode project

---
+++ BudgetBudget/BudgetBudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetBudgetApp.swift
+//  BudgetBudget
+//
+//  Created by Leo Benz on 17.07.22.
+//
+
+import SwiftUI
+
+@main
+struct BudgetBudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
